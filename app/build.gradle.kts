34ddb10d69098ce885675eaf4b6d4e5490733e21
qawse110 <qawse110@users.noreply.github.com>
2026-08-23 00:18:18 +0800

    chore: bump versionCode 10 / 4.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.launcher"
         minSdk = 24
         targetSdk = 28
-        versionCode = 9
-        versionName = "4.2.2"
+        versionCode = 10
+        versionName = "4.2.3"
     }
 
     buildTypes {
